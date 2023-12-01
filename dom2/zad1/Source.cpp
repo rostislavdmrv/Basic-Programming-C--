d57ebc7a02174d32fa3b238800d9d1b292dcf745
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2023-12-01 16:26:10 +0200

    cout

---
+++ dom2/zad1/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Ranking in a rhythmic gymnastics competition:" << endl;
+	
+		cout << " 1.Rank Gymnast  Dina Averina      Nation Russia   Ribbon 21.650   Ball 22.950   Batons 23.000  Hoop 23.800   Total 91.400 " << endl;
+		cout << " 2.Rank Gymnast  Arina Averina     Nation Russia   Ribbon 20.850   Ball 23.100   Batons 24.050  Hoop 23.100   Total 91.100 " << endl;
+		cout << " 3.Rank Gymnast  Linoy Ashra       Nation Isreal   Ribbon 21.050   Ball 23.100   Batons 23.500  Hoop 22.050   Total 89.700 " << endl;
+		cout << " 4.Rank Gymnast  Boryana Kaleyn    Nation Bulgaria Ribbon 19.900   Ball 22.400   Batons 22.350  Hoop 21.625   Total 96.700 " << endl;
+		cout << " 5.Rank Gymnast  Vlada Nikolchenko Nation Ukraine  Ribbon 19.450   Ball 22.250   Batons 19.500  Hoop 22.950   Total 84.150 " << endl;
+		return 0;
+}
