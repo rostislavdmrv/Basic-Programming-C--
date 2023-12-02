e83ec673b9b866725d762f4782e85b74673bfc34
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2023-12-02 17:56:57 +0200

    2

---
+++ dom1/zad2/Source2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter a:" << endl;
+	double a;
+	cin >> a;
+	cout << "Enter h:" << endl;
+	double h;
+	cin >> h;
+	double s;
+	s = (a*h)/1/2.0;
+		cout <<"S of triangle is " << s << endl;
+}
