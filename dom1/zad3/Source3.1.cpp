efe94d76a2299df7e2a2e480c1fc6114b8a310fd
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2023-12-02 17:59:04 +0200

    3

---
+++ dom1/zad3/Source3.1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter a :";
+	double a;
+	cin >> a;
+	cout << "Enter b :";
+	double b;
+	cin >> b;
+	double P;
+	P = 2 * a + 2 * b;
+	double S;
+	S = a * b;
+	cout << "P=" << P << " and  " << "S=" << S << endl;
+
+
+}
