7b1002b612377b4b6cf52bf35a1cc7746a0cd56f
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2023-12-03 16:17:10 +0200

    6

---
+++ dom1/zad6/Source6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char a;
+	cout << "Enter a character : ";
+	cin >> a;
+	cout << "ASCII value of " << a << " is : " << (int)a;
+	return 0;
+}
