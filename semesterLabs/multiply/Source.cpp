fef91f3d7cd8e4dfbe9f61aea8822462587e651c
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-06-26 21:25:58 +0300

    multiply

---
+++ semesterLabs/multiply/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//int main()
+//{
+//	setlocale(LC_ALL, "BG");
+//	int num;
+//	unsigned long int pro = 1;
+//	cout << "Въведете целите числа или 0 за изход  " << endl;
+//	cin >> num;
+//	while (num!=0 )
+//	{
+//		if (num > 0)
+//			pro *= num;
+//		cin >> num;
+//
+//	}
+//	cout << "Произведението е  "<< pro << endl;
+//}
+
